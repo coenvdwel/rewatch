4e1e665e0e85233a2def7293f523442617c73bcc
Sebastian L <bitbucket@lorr-and-order.com>
2024-07-27 22:51:57 +0200

    Deploy-script for development prupose. overworked creation of deployable (runnable at git-bash)

--- 7z.sh
+++ 7z.sh
@@ -1,2 +1,20 @@
+#!/bin/bash
+echo "7z archive creation started...."
+# Load configuration variables
+source ./config.sh
+
+# Change to the parent directory of the current script
 cd ..
-"C:\Program Files\7-Zip\7z.exe" a Rewatch/Rewatch_80004.zip Rewatch/*.lua Rewatch/*.toc Rewatch/assets Rewatch/src
+
+echo "Current directory: $(pwd)"
+
+# Convert the Windows path to a Git Bash compatible path
+SEVEN_ZIP_PATH="/c/Program Files/7-Zip/7z.exe"
+
+# Create an archive of the Rewatch folder
+"$SEVEN_ZIP_PATH" a "Rewatch/Rewatch_$REWATCH_VERSION.zip" "Rewatch/*.lua" "Rewatch/*.toc" "Rewatch/assets" "Rewatch/src"
+
+# Change back to the Rewatch directory
+cd $REWATCH_SOURCE
+
+echo "7z archive creation complete."
---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Load configuration variables
+source ./config.sh
+
+# Display the current operation
+echo "Setting up paths and version number..."
+echo "WOW AddOn Folder: $WOW_ADDON_FOLDER"
+echo "Rewatch Version: $REWATCH_VERSION"
+echo "7-Zip Path: $SEVEN_ZIP_PATH"
+
+# Call the 7z.sh script
+echo "Running 7z.sh to create the archive..."
+./7z.sh
+
+# Check if 7z.sh executed successfully
+if [ $? -eq 0 ]; then
+    echo "7z.sh executed successfully."
+else
+    echo "Error: 7z.sh failed."
+    exit 1
+fi
+
+# Clean existing files in the WoW AddOn folder
+echo "Cleaning existing files in the WoW AddOn folder..."
+rm -rfv "$WOW_ADDON_FOLDER/Rewatch"
+echo "Clean up complete."
+
+# Wait for 10 seconds
+echo "Pausing for 1 seconds..."
+sleep 1
+echo "Resuming operations."
+
+# Change to the WoW AddOn folder
+#echo "Changing to the WoW AddOn folder..."
+#cd "$WOW_ADDON_FOLDER"
+#echo "Current directory: $(pwd)"
+
+# Deploy the Rewatch AddOn by extracting the zip file to the AddOn folder
+echo "Extracting Rewatch AddOn version $REWATCH_VERSION..."
+"$SEVEN_ZIP_PATH" x "$REWATCH_SOURCE/Rewatch_$REWATCH_VERSION.zip" -o"$WOW_ADDON_FOLDER" -y
+echo "Extraction complete."
+
+# Wait for user input before closing
+echo "Deployment finished. Press Enter to exit..."
+read -p ""
